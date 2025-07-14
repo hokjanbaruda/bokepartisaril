78d7c39a5ee21f1bbefd5c8d1413e535beca3c58
hokjanbaruda <hokjanbaruda@gmail.com>
2025-07-14 21:59:03 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Artis Aril';
-export const desk = 'Video Bokep Indo Jepang Jav Hd Artis Viral Terbaru Bocil Ngentot Jilbab Smp Mama Smp';
-export const url = 'https://bokepartisaril.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
